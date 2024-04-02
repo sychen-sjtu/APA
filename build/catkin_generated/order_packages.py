e7536dc91dde786ebd9098bb477b0f0b799388e7
sychen-sjtu <779691662@qq.com>
2024-04-02 10:27:32 +0800

    add gitignore

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/sychen/Project/APA/APA/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
